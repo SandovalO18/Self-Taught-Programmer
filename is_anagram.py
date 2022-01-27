13199b22a82474b1e0fb666d586769b5fdff9237
SandovalO18 <sandoval260111@gmail.com>
2022-01-27 08:00:46 -0600

    new commit

---
+++ is_anagram.py
@@ -0,0 +1,6 @@
+def is_anagram(w1, w2):
+    w1 = w1.lower()
+    w2 = w2.lower()
+    return sorted(w1) == sorted(w2)
+
+is_anagram("iceman","cinema")
---
+++ is_palindrome.py
@@ -0,0 +1,5 @@
+def is_palindrome(word):
+    word = word.lower()
+    return word[::-1] == word
+
+is_palindrome("mom")
