7e844784c48318d37984ce3e69d90c46403819ac
SandovalO18 <sandoval260111@gmail.com>
2022-01-28 14:42:45 -0600

    adding new ones

--- stack.py
+++ stack.py
@@ -17,3 +17,25 @@ class Stack:
 
     def size(self):
         return len(self.items)
+
+stack = Stack()
+
+for c in "Hello":
+    stack.push(c)
+reversed_string = ""
+for i in range(len(stack.items)):
+    reversed_string += stack.pop()
+print(reversed_string)
+
+list1 = [1, 2, 3, 4, 5]
+list2 = []
+
+stack = Stack()
+for item in list1:
+    stack.push(item)
+
+
+for i in range(len(stack.items)):
+    list2.append(stack.pop())
+
+print(list2)
